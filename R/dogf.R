9fa593389938b467c791eca5acb489083a59afeb
vjcitn <stvjc@channing.harvard.edu>
2024-07-03 13:25:16 -0400

    use cache

--- R/dogf.R
+++ R/dogf.R
@@ -37,3 +37,26 @@ getgf = function() {
 #' @export
 panc_tok_zip_url = function()
 "https://mghp.osn.xsede.org/bir190004-bucket01/BiocGFdata/panc_hfds.zip"
+
+#' retrieve or place a zipfile with tokenization dataset for Grun/Muraro's pancreas study in BiocFileCache
+#' @import BiocFileCache
+#' @note A loom file was distributed at
+#' \url{https://figshare.com/articles/dataset/Data_used_for_demo_of_the_code_accompanying_the_i_Assessing_the_limits_of_zero-shot_foundation_models_in_single-cell_biology_i_paper_/24747228} under
+#' data/datasets/geneformer/pancreas_scib.  The relevant publication
+#' seems to be
+#'  \url{https://pubmed.ncbi.nlm.nih.gov/27345837/}, GSE81076, etc.
+#' @return character path to zip file
+#' @export
+get_panc_tok_path = function(cache = BiocFileCache::BiocFileCache()) {
+  src = panc_tok_zip_url()
+  q = BiocFileCache::bfcquery(cache, "panc_hfds.zip")
+  nans = nrow(q)
+  if (nans >= 1) {
+    q = q[nans,]
+    return(q$rpath)  # quietly return last
+    }
+  p = BiocFileCache::bfcadd(cache, rname=src,
+    action="copy", download=TRUE)
+  p
+  }
+  
