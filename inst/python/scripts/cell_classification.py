a3b42582903f425db58ef0f9e15acfefa0f65f66
vjcitn <stvjc@channing.harvard.edu>
2024-07-04 10:28:09 +0000

    add munged script for run on jetstream2, 10GB GPU RAM seem inadequate

---
+++ inst/python/scripts/cell_classification.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# MUNGED FROM BASIC SOURCES TO RUN ON JETSTREAM2 WITH LARGE DISK
+
+# ## Geneformer Fine-Tuning for Classification of Cardiomyopathy Disease States
+
+# ### Please note that, as usual with deep learning models, we **highly** recommend tuning learning hyperparameters for all fine-tuning applications as this can significantly improve model performance. Example below uses previously optimized hyperparameters, but one can optimize hyperparameters with the argument n_hyperopt_trials=n in cc.validate() where n>0 and represents the number of trials for hyperparameter optimization.
+
+# ### Train cell classifier with 70% of data (with hyperparameters previously optimized based on 15% of data as validation set) and evaluate on held-out test set of 15% of data
+
+# ### Fine-tune the model for cell state classification
+
+# In[2]:
+
+
+import datetime
+from geneformer import Classifier
+
+current_date = datetime.datetime.now()
+datestamp = f"{str(current_date.year)[-2:]}{current_date.month:02d}{current_date.day:02d}{current_date.hour:02d}{current_date.minute:02d}{current_date.second:02d}"
+datestamp_min = f"{str(current_date.year)[-2:]}{current_date.month:02d}{current_date.day:02d}"
+
+output_prefix = "cm_classifier_test"
+output_dir = f"/media/volume/boot-vol-vince_12apr/vjcoutput2"
+
+
+# In[3]:
+
+
+filter_data_dict={"cell_type":["Cardiomyocyte1","Cardiomyocyte2","Cardiomyocyte3"]}
+training_args = {
+    "num_train_epochs": 0.9,
+    "learning_rate": 0.000804,
+    "lr_scheduler_type": "polynomial",
+    "warmup_steps": 1812,
+    "weight_decay":0.258828,
+    "per_device_train_batch_size": 12,
+    "seed": 73,
+}
+cc = Classifier(classifier="cell",
+                cell_state_dict = {"state_key": "disease", "states": "all"},
+                filter_data=filter_data_dict,
+                training_args=training_args,
+                max_ncells=None,
+                freeze_layers = 2,
+                num_crossval_splits = 1,
+                forward_batch_size=200,
+                nproc=16)
+
+
+# In[4]:
+
+
+# previously balanced splits with prepare_data and validate functions
+# argument attr_to_split set to "individual" and attr_to_balance set to ["disease","lvef","age","sex","length"]
+train_ids = ["1447", "1600", "1462", "1558", "1300", "1508", "1358", "1678", "1561", "1304", "1610", "1430", "1472", "1707", "1726", "1504", "1425", "1617", "1631", "1735", "1582", "1722", "1622", "1630", "1290", "1479", "1371", "1549", "1515"]
+eval_ids = ["1422", "1510", "1539", "1606", "1702"]
+test_ids = ["1437", "1516", "1602", "1685", "1718"]
+
+train_test_id_split_dict = {"attr_key": "individual",
+                            "train": train_ids+eval_ids,
+                            "test": test_ids}
+
+# Example input_data_file: https://huggingface.co/datasets/ctheodoris/Genecorpus-30M/tree/main/example_input_files/cell_classification/disease_classification/human_dcm_hcm_nf.dataset
+cc.prepare_data(input_data_file="/media/volume/boot-vol-vince_12apr", #"human_dcm_hcm_nf.dataset",
+                output_directory=output_dir,
+                output_prefix=output_prefix,
+                split_id_dict=train_test_id_split_dict)
+
+
+# In[5]:
+
+
+train_valid_id_split_dict = {"attr_key": "individual",
+                            "train": train_ids,
+                            "eval": eval_ids}
+
+# 6 layer Geneformer: https://huggingface.co/ctheodoris/Geneformer/blob/main/model.safetensors
+all_metrics = cc.validate(model_directory="/home/exouser/Geneformer",
+                          prepared_input_data_file=f"{output_dir}/{output_prefix}_labeled_train.dataset",
+                          id_class_dict_file=f"{output_dir}/{output_prefix}_id_class_dict.pkl",
+                          output_directory=output_dir,
+                          output_prefix=output_prefix,
+                          split_id_dict=train_valid_id_split_dict)
+                          # to optimize hyperparameters, set n_hyperopt_trials=100 (or alternative desired # of trials)
+
+
+# ### Evaluate the model
+
+# In[1]:
+
+
+cc = Classifier(classifier="cell",
+                cell_state_dict = {"state_key": "disease", "states": "all"},
+                forward_batch_size=200,
+                nproc=16)
+
+
+# In[2]:
+
+
+all_metrics_test = cc.evaluate_saved_model(
+        model_directory=f"{output_dir}/{datestamp_min}_geneformer_cellClassifier_{output_prefix}/ksplit1/",
+        id_class_dict_file=f"{output_dir}/{output_prefix}_id_class_dict.pkl",
+        test_data_file=f"{output_dir}/{output_prefix}_labeled_test.dataset",
+        output_directory=output_dir,
+        output_prefix=output_prefix,
+    )
+
+
+# In[3]:
+
+
+cc.plot_conf_mat(
+        conf_mat_dict={"Geneformer": all_metrics_test["conf_matrix"]},
+        output_directory=output_dir,
+        output_prefix=output_prefix,
+        custom_class_order=["nf","hcm","dcm"],
+)
+
+
+# In[4]:
+
+
+cc.plot_predictions(
+    predictions_file=f"{output_dir}/{output_prefix}_pred_dict.pkl",
+    id_class_dict_file=f"{output_dir}/{output_prefix}_id_class_dict.pkl",
+    title="disease",
+    output_directory=output_dir,
+    output_prefix=output_prefix,
+    custom_class_order=["nf","hcm","dcm"],
+)
+
+
+# In[5]:
+
+
+all_metrics_test
+
